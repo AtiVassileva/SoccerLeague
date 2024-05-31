d0e5295ec2306576e480a945af936c8a5832b0ae
Ati <ativassileva@gmail.com>
2024-05-31 18:28:41 +0300

    Introduced base entity

---
+++ SoccerLeague/SoccerLeague.Data/Models/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoccerLeague.Data.Models
+{
+    public class BaseEntity
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+    }
+}
