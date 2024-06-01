b35ae87f87c1400f6497a4e17da226b643cf818b
Ati <ativassileva@gmail.com>
2024-06-01 09:15:57 +0300

    Added db sets

--- SoccerLeague/SoccerLeague.Data/ApplicationDbContext.cs
+++ SoccerLeague/SoccerLeague.Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SoccerLeague.Data.Models;
 
 namespace SoccerLeague.Data
 {
@@ -8,6 +9,22 @@ namespace SoccerLeague.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Arena> Arenas { get; set; } = null!;
+        public DbSet<League> Leagues { get; set; } = null!;
+        public DbSet<Match> Matches { get; set; } = null!;
+        public DbSet<Player> Players { get; set; } = null!;
+        public DbSet<Team> Teams { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("DefaultConnection");
+            }
+
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
