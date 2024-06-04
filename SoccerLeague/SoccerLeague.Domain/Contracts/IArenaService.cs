1442667159545847a080d115b6c02b4412de155f
Ati <ativassileva@gmail.com>
2024-06-04 16:09:37 +0300

    Added arena service interface

---
+++ SoccerLeague/SoccerLeague.Domain/Contracts/IArenaService.cs
@@ -0,0 +1,15 @@
+﻿using SoccerLeague.Models.Data;
+using SoccerLeague.Models.Request;
+using SoccerLeague.Models.Response;
+
+namespace SoccerLeague.Domain.Contracts
+{
+    public interface IArenaService
+    {
+        IEnumerable<Arena> GetAll();
+        IEnumerable<ArenaResponseModel> GetById(Guid id);
+        bool Create(ArenaRequestModel model);
+        bool Edit(Guid id, ArenaRequestModel model);
+        bool Delete(Guid id);
+    }
+}
---
+++ SoccerLeague/SoccerLeague.Models/Response/ArenaResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SoccerLeague.Models.Response
+{
+    public class ArenaResponseModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public Guid HomeTeamId { get; set; }
+    }
+}
