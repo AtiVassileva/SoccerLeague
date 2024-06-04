29467fff5b9c84bd4e3f2cb612bd2973207f13d8
Ati <ativassileva@gmail.com>
2024-06-04 09:51:28 +0300

    Added logo to leagues

---
+++ SoccerLeague/SoccerLeague.Data/Migrations/20240604065023_AddedLogoToLeagues.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoccerLeague.UI.Data.Migrations
+{
+    public partial class AddedLogoToLeagues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrl",
+                table: "Leagues",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoUrl",
+                table: "Leagues");
+        }
+    }
+}
--- SoccerLeague/SoccerLeague.Data/Models/League.cs
+++ SoccerLeague/SoccerLeague.Data/Models/League.cs
@@ -2,5 +2,6 @@
 {
     public class League : BaseEntity
     {
+        public string? LogoUrl { get; set; }
     }
 }
